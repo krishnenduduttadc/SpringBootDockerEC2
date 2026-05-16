034688e75aeefa8dd0cbcde6b26fef1e4a4768e4
krishnendu <krishnenduduttadc@gmail.com>
2026-05-16 18:38:11 +0530

    initial

---
+++ src/main/java/com/example/demoEC2/DemoEc2Application.java
@@ -0,0 +1,13 @@
+package com.example.demoEC2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEc2Application.class, args);
+	}
+
+}
